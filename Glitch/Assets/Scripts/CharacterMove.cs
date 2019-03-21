bdf73045d974c6500a20cb886a15fafaf8a0c6b4
Robert Badea <owlree128@gmail.com>
2019-03-22 00:11:52 +0200

    Remove Start() method

--- Glitch/Assets/Scripts/CharacterMove.cs
+++ Glitch/Assets/Scripts/CharacterMove.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     void FixedUpdate()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(
