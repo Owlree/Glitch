638189c1eed398959fae238c21549453143f2002
Robert Badea <owlree128@gmail.com>
2019-03-22 00:07:34 +0200

    Dog can walk

---
+++ Glitch/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+}
