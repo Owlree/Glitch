1ee10c279915b441b944d426dd597c69ef941565
Robert Badea <owlree128@gmail.com>
2019-03-22 21:46:05 +0200

    Add FollowObject2D script

---
+++ Glitch/Assets/Scripts/FollowObject2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject follow;
+
+    void LateUpdate()
+    {
+        float myX = transform.position.x;
+        float followX = follow.transform.position.x;
+
+        if (myX < followX + 1 || myX > followX + 3)
+        {
+            transform.position = new Vector3(
+                Mathf.Clamp(myX, followX + 1, followX + 3),
+                transform.position.y,
+                transform.position.z);
+        }
+    }
+}
