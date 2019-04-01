662056fdd8a8934f4af82dc2fb1af1da074d0fd0
Robert Badea <owlree128@gmail.com>
2019-04-01 23:53:44 +0300

    Remove redundant condition

--- Glitch/Assets/Scripts/FollowObject2D.cs
+++ Glitch/Assets/Scripts/FollowObject2D.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollow: MonoBehaviour
 {
     public GameObject follow;
 
@@ -11,12 +11,9 @@ public class CameraFollow : MonoBehaviour
         float myX = transform.position.x;
         float followX = follow.transform.position.x;
 
-        if (myX < followX + 1 || myX > followX + 3)
-        {
-            transform.position = new Vector3(
-                Mathf.Clamp(myX, followX + 1, followX + 3),
-                transform.position.y,
-                transform.position.z);
-        }
+        transform.position = new Vector3(
+            Mathf.Clamp(myX, followX + 1, followX + 3),
+            transform.position.y,
+            transform.position.z);
     }
 }
